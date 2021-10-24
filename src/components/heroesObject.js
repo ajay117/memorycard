d405d17586287eecc7b2d4c214496efd6a71bdae
Ajay KC <meajay64@gmail.com>
2021-10-24 16:09:06 +0900

    Create an  object with name and image location of heroes

---
+++ src/components/heroesObject.js
@@ -0,0 +1,85 @@
+import batman from "../images/batman.jpeg";
+import blackPanther from "../images/black-panther.jpeg";
+import captainAmerica from "../images/captain-america.jpeg";
+import captainMarvel from "../images/captain-marvel.jpeg";
+import cyclops from "../images/cyclops.jpeg";
+import doctorStrange from "../images/doctor-strange.jpeg";
+import flash from "../images/flash.jpeg";
+import greenLantern from "../images/green-lantern.jpeg";
+import hulk from "../images/hulk.jpeg";
+import jean from "../images/jean.jpeg";
+import john from "../images/john.jpeg";
+import storm from "../images/storm.jpeg";
+import superman from "../images/superman.jpeg";
+import thor from "../images/thor.jpeg";
+import wolverine from "../images/wolverine.jpeg";
+import wonderWoman from "../images/wonder-women.jpeg";
+
+const heroesObject = [
+	{
+		name: "Batman",
+		src: batman,
+	},
+	{
+		name: "Black Panther",
+		src: blackPanther,
+	},
+	{
+		name: "Captain America",
+		src: captainAmerica,
+	},
+	{
+		name: "Captain Marvel",
+		src: captainMarvel,
+	},
+	{
+		name: "Cyclops",
+		src: cyclops,
+	},
+	{
+		name: "Doctor Strange",
+		src: doctorStrange,
+	},
+	{
+		name: "Flash",
+		src: flash,
+	},
+	{
+		name: "Green Lantern",
+		src: greenLantern,
+	},
+	{
+		name: "Hulk",
+		src: hulk,
+	},
+	{
+		name: "Jean",
+		src: jean,
+	},
+	{
+		name: "John",
+		src: john,
+	},
+	{
+		name: "Storm",
+		src: storm,
+	},
+	{
+		name: "Superman",
+		src: superman,
+	},
+	{
+		name: "Thor",
+		src: thor,
+	},
+	{
+		name: "Wolverine",
+		src: wolverine,
+	},
+	{
+		name: "Wonder Woman",
+		src: wonderWoman,
+	},
+];
+
+export default heroesObject;
