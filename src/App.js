9860496f224e199f70a58fd172a80d75f39e67fc
Ajay KC <meajay64@gmail.com>
2021-12-01 22:25:03 +0900

    Added code and deleted some unused files

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { render } from "@testing-library/react";
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import heroesObject from "./components/heroesObject";
+import ImageLoad from "./components/ImageLoad";
 
 function App() {
     const [heroesChoseByPlayer, setHeroesChoseByPlayer] = useState([]);
---
+++ src/components/ImageLoad.js
@@ -0,0 +1,18 @@
+import { useState,useEffect } from "react";
+import heroesObject from "./heroesObject";
+
+function ImageLoad(props) {
+    const [loadHeroesArr, setLoadHeroesArr] = useState(heroesObject[0].src);
+    useEffect(() => {
+        while(loadHeroesArr.length <= 5) {
+            let randomNum = Math.floor(Math.random() * 17);
+        }
+    },[]);
+    return (
+        <div>
+            <img src={loadHeroesArr} alt="hero" />
+        </div>
+    );
+}
+
+export default ImageLoad;
