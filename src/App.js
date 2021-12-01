edd5c2d151699e887148458501fa0c71cfc1da3b
Ajay KC <meajay64@gmail.com>
2021-12-01 20:52:48 +0900

    Deleted code as the game format changes

--- src/App.js
+++ src/App.js
@@ -4,35 +4,7 @@ import React, { useState } from "react";
 import "./App.css";
 import heroesObject from "./components/heroesObject";
 
-// function App() {
-// 	const [score, setScore] = useState(0);
-// 	const [highScore, setHighScore] = useState(0);
-// 	const [level, setLevel] = useState(1);
-// 	//Pictures we have clicked will be saved in this array.
-// 	const [seenImages, setSeenImages] = useState([]);
-// 	// const [randomIndex, setRandomIndex] = useState();
-// 	const [hero, setHero] = useState(heroesObject[Math.floor(Math.random() * 16)]);
-
-
-
-
-// 	return (
-// 		<div className="game-container">
-// 			<h4>High Score: {highScore}</h4>
-// 			<h4>Score: {score}</h4>
-// 			<h4>Level: {level}</h4>
-// 			<h5>SuperHero: {hero.name}</h5>
-// 			<div className="img-container">
-// 			  <img src={hero.src} alt="Marvel hero"/>
-// 			</div>
-// 			<div className="btn-container">
-// 				<button onClick={sameImage}>Repeated</button>
-// 				<button>Not Repeated</button>
-// 				<button>Play Again</button>
-// 			</div>
-// 			<p className="info">Score 5 points to level up.</p>
-// 		</div>
-// 	);
+function App() {
 }
 
 export default App;
