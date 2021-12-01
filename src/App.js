bb3523049146a5485df5971c35483df5661695a0
Ajay KC <meajay64@gmail.com>
2021-12-01 21:18:55 +0900

    Initital state created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-// import React, { useState, useEffect } from "react";
-// import { render } from "@testing-library/react";
-import React, { useState } from "react";
+import { render } from "@testing-library/react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import heroesObject from "./components/heroesObject";
 
 function App() {
+    const [heroesChoseByPlayer, setHeroesChoseByPlayer] = useState([]);
+    const [score, setScore] = useState(0);
+    const [highScore, setHighScore] = useState(0);
+    const [level, setLevel] = useState(0);
+    return (
+        <ImageLoad />
+    );
 }
 
 export default App;
