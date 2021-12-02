4a45dbbd1e228d9db292ecffed1054b82b251f64
Ajay KC <meajay64@gmail.com>
2021-12-02 21:00:49 +0900

    State and event listener lifted from child component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import { render } from "@testing-library/react";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import heroesObject from "./components/heroesObject";
+// import heroesObject from "./components/heroesObject";
 import PlayArea from "./components/PlayArea";
 
 function App() {
-    const [heroesChoseByPlayer, setHeroesChoseByPlayer] = useState([]);
     const [score, setScore] = useState(0);
-    const [highScore, setHighScore] = useState(0);
-    const [level, setLevel] = useState(0);
+    const heroes = [heroesObject[0], heroesObject[1]];
+    const clicked = () => {
+        setScore(score + 1);
+    }
+    // const [heroesChoseByPlayer, setHeroesChoseByPlayer] = useState([]);
+    // const [highScore, setHighScore] = useState(0);
+    // const [level, setLevel] = useState(0);
     return (
-        <PlayArea />
+        <PlayArea heroes={heroes} score={score} clicked={clicked} />
     );
 }
 
