1aea26a0e9554a350d5cda657e8387a16c09a2d0
Ajay KC <meajay64@gmail.com>
2021-12-02 23:13:47 +0900

    Imported other components and now pics are auto generated and score is updating

--- src/App.js
+++ src/App.js
@@ -1,20 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import heroesObject from "./components/heroesObject";
 // import heroesObject from "./components/heroesObject";
 import PlayArea from "./components/PlayArea";
+import GameOver from "./components/GameOver";
+import GameInfo from "./components/GameInfo";
 
 function App() {
+    const [isGameOver, setIsGameOver] = useState(false);
     const [score, setScore] = useState(0);
-    const heroes = [heroesObject[0], heroesObject[1]];
-    const clicked = () => {
-        setScore(score + 1);
+    const [highScore, setHighScore] = useState(0);
+    const heroes = [];
+    const [playerClickedPics, setPlayerClickedPics] = useState([]);
+    
+    let randomNumArr = [];
+    while(randomNumArr.length < 6) {
+      let randomNum = Math.floor(Math.random() * 16);
+      if(!randomNumArr.includes(randomNum)) {
+          randomNumArr.push(randomNum);
+      } 
+    }
+
+    randomNumArr.forEach(num => {
+        heroes.push(heroesObject[num]);
+    })
+
+
+
+    const clicked = (e) => {
+        let heroName = e.target.dataset.name;
+        if(!playerClickedPics.includes(heroName)) {
+          setPlayerClickedPics([...playerClickedPics, e.target.dataset.name]);
+          setScore(score + 1);
+        } else {
+            setIsGameOver(true);
+            if(score > highScore) {
+                setHighScore(score);
+            }
+        }
     }
     // const [heroesChoseByPlayer, setHeroesChoseByPlayer] = useState([]);
-    // const [highScore, setHighScore] = useState(0);
     // const [level, setLevel] = useState(0);
     return (
-        <PlayArea heroes={heroes} score={score} clicked={clicked} />
+        <div>
+            <GameInfo score={score}
+                      highScore={highScore}/>
+            {!isGameOver ? <PlayArea
+                            heroes={heroes}
+                            score={score} 
+                            clicked={clicked} /> 
+                         : <GameOver />}
+        </div>
     );
 }
 
