cd711c8aaf9b2ceae8b8fe2edb0d6e5331153a7c
Ajay KC <meajay64@gmail.com>
2021-12-02 19:26:53 +0900

    Initial code started

--- src/components/PlayArea.js
+++ src/components/PlayArea.js
@@ -1,16 +1,14 @@
-import { useState,useEffect } from "react";
+// import { useState } from "react";
 import heroesObject from "./heroesObject";
 
 function PlayArea(props) {
-    const [loadHeroesArr, setLoadHeroesArr] = useState(heroesObject[0].src);
-    useEffect(() => {
-        while(loadHeroesArr.length <= 5) {
-            let randomNum = Math.floor(Math.random() * 17);
-        }
-    },[]);
+    const heroes = [heroesObject[0], heroesObject[1]];
+    let addHeroes = heroes.map(hero => 
+        <img src={hero.src} alt= {hero.name} />
+    );
     return (
         <div>
-            <img src={loadHeroesArr} alt="hero" />
+            {addHeroes};
         </div>
     );
 }
