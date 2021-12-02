239a29b4206c86825beb13ec444058321363d4a7
Ajay KC <meajay64@gmail.com>
2021-12-02 20:51:37 +0900

    Coded simple logic

--- src/components/PlayArea.js
+++ src/components/PlayArea.js
@@ -1,14 +1,24 @@
-// import { useState } from "react";
+import { useState } from "react";
 import heroesObject from "./heroesObject";
+import GameWon from "./GameWon";
 
 function PlayArea(props) {
     const heroes = [heroesObject[0], heroesObject[1]];
+    const [score, setScore] = useState(0);
+    const clicked = () => {
+        setScore(score + 1);
+    }
     let addHeroes = heroes.map(hero => 
-        <img src={hero.src} alt= {hero.name} />
+        <img src={hero.src} onClick={clicked} alt= {hero.name} key={hero.name} />
     );
     return (
         <div>
-            {addHeroes};
+            <p>Score: {score}</p>
+            <p>High Score: </p>
+            <p>Level</p>
+            <div>
+                {(score === 5) ? <GameWon /> : addHeroes};
+            </div>
         </div>
     );
 }
