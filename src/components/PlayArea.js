77f876ce7f9e8be82af8c9637caf8a6029e4883c
Ajay KC <meajay64@gmail.com>
2021-12-03 22:35:52 +0900

    Initial Game Logic works now

--- src/components/PlayArea.js
+++ src/components/PlayArea.js
@@ -1,12 +1,22 @@
-// import { useState } from "react";
+import { useEffect } from "react"; 
 // import heroesObject from "./heroesObject";
 import GameWon from "./GameWon";
 
 function PlayArea(props) {
+    useEffect(() => {
+        let heroContainer = document.querySelector('.hero-container');
+        let fadeEffect = setTimeout(() => {
+                             heroContainer.classList.add('full-opacity');
+                         },300);
+        return () => {
+            heroContainer.classList.remove('full-opacity');
+            clearTimeout(fadeEffect);
+        }
+    });
     let addHeroes = props.heroes.map(hero => 
-        <div data-name={hero.name} onClick={props.clicked} key={hero.name}>
-          <img data-name={hero.name} src={hero.src}  alt= {hero.name} />
-          <p data-name={hero.name}>{hero.name}</p>
+        <div className="hero" data-name={hero.name} onClick={props.clicked} key={hero.name}>
+          <img className="hero-img" data-name={hero.name} src={hero.src}  alt= {hero.name} />
+          <p className="hero-name" data-name={hero.name}>{hero.name}</p>
         </div>
     );
     return (
